05d6c218d36164e97afa1f181e1ae3ac2f5774db
Victor Geraldsson <victor.geraldsson@lime.tech>
2024-12-13 00:04:20 +0100

    day 9 part two

--- day09.ts
+++ day09.ts
@@ -9,11 +9,14 @@ type Block = FileBlock | FreeSpaceBlock;
 
 const compactedBlocks = moveFileBlocks(readDiskMap(diskMap));
 
-const checksum = compactedBlocks
+const checksum = (blocks: Block[]) => blocks
     .map((block, pos) => (block || 0) * pos)
     .reduce((a, b) => a + b);
 
-console.log(checksum);
+console.log(checksum(compactedBlocks));
+
+console.log("Part 2");
+console.log(checksum(moveWholeFiles(readDiskMap(diskMap))));
 
 function readDiskMap(digits: string) {
     const blocks: Block[] = [];
@@ -47,4 +50,63 @@ function moveFileBlocks(blocks: Block[]) {
     }
 
     return blocks;
+}
+
+function moveWholeFiles(blocks: Block[]) {
+    const blocksToMove = blocks.slice();
+
+    while (blocksToMove.includes('')) {
+        const lastFilePos = findLastFile(blocksToMove);
+        if (lastFilePos === -1) {
+            break;
+        }
+
+        const len = getFileLength(blocksToMove, lastFilePos);
+        const removed = blocksToMove.splice(lastFilePos);
+
+        const freePos = findFreeSpace(blocks, len);
+
+        if (freePos === -1 || freePos >= lastFilePos) {
+            continue;
+        }
+
+        blocks.splice(freePos, len, ...removed.slice(0, len));
+        blocks.splice(lastFilePos, len, ...new Array(len).fill(''));
+    }
+
+    return blocks;
+}
+
+function findLastFile(blocks: Block[]) {
+    return blocks.findLastIndex(
+        (block, index) => block !== '' && index > 0 && block !== blocks[index - 1]
+    )
+}
+
+function getFileLength(blocks: Block[], pos: number) {
+    let len = 1;
+
+    while (blocks[pos] === blocks[pos + 1]) {
+        pos++;
+        len++;
+    }
+
+    return len;
+}
+
+function findFreeSpace(blocks: Block[], len: number) {
+    let pos = 0;
+
+    while ((pos = blocks.indexOf('', pos)) !== -1) {
+        let count = 1;
+        while (count < len && blocks[pos + count] === '') {
+            count++;
+        }
+        if (count === len) {
+            return pos;
+        }
+        pos++;
+    }
+
+    return -1;
 }
