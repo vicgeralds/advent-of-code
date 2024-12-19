35ddf4ae6517c7fa76100ce4141f64521f5fc50d
Victor Geraldsson <victor.geraldsson@lime.tech>
2024-12-20 00:01:27 +0100

    day 13 part two

--- day13.sh
+++ day13.sh
@@ -7,7 +7,23 @@ total_tokens=0
 for line in $input; do
     claw_machine_data="${claw_machine_data}${line};"
     if [[ "$line" == prize* ]]; then
-        output=$(minizinc --solver cbc day13.mzn -D "$claw_machine_data")
+        eval "$claw_machine_data"
+        (( prize_x += 10000000000000 ))
+        (( prize_y += 10000000000000 ))
+        a1=$((button_b_y * prize_x - button_b_x * prize_y))
+        a2=$((button_b_y * button_a_x - button_b_x * button_a_y))
+        b1=$((button_a_x * prize_y - button_a_y * prize_x))
+        b2=$((button_a_x * button_b_y - button_a_y * button_b_x))
+
+        if (( a1 % a2 == 0 )) && (( b1 % b2 == 0)); then
+            a=$((a1 / a2))
+            b=$((b1 / b2))
+            if (( a >= 0 )) && (( b >= 0 )); then
+                (( total_tokens += 3*a + b ))
+            fi
+        fi
+        #output=$(minizinc --solver cbc day13.mzn -D "$claw_machine_data")
+        output=""
         if [[ "$output" == *tokens:* ]]; then
             for tokens in ${output#*tokens:}; do
                 echo "adding tokens: $tokens"
